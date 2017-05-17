5be4381a5698a99788e43fa4cd5c2572a5b30a81
Michele Colledanchise <miccol@kth.se>
2017-05-17 18:47:15 +0200

    removed unused objects in main

--- main.cpp
+++ main.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
-#include<behavior_tree.h>
-using namespace std;
-
+#include <behavior_tree.h>
 
 
 
 class MyCondition : public BT::ConditionNode
 {
 public:
-    explicit MyCondition(std::string name);
+    MyCondition(std::string name);
+    ~MyCondition();
     BT::ReturnStatus Tick();
 };
 
 MyCondition::MyCondition(std::string name) : BT::ConditionNode::ConditionNode(name)
 {
 
-
 }
 
 BT::ReturnStatus MyCondition::Tick()
 {
-    cout << "Checking!" << endl;
+    std::cout << "The Condition is true" << std::endl;
+
     return BT::SUCCESS;
 }
 
@@ -29,6 +28,7 @@ class MyAction : public BT::ActionNode
 {
 public:
     MyAction(std::string name);
+    ~MyAction();
     BT::ReturnStatus Tick();
     void Halt();
 };
@@ -39,47 +39,51 @@ MyAction::MyAction(std::string name) : ActionNode::ActionNode(name)
 }
 
 
-
-
-
-
 BT::ReturnStatus MyAction::Tick()
 {
+    std::cout << "The Action is doing some operations" << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    if (is_halted())
+    {
+        return BT::HALTED;
+    }
 
-//    int i = 0;
+    std::cout << "The Action is doing some others operations" << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    if (is_halted())
+    {
+        return BT::HALTED;
+    }
 
-//    while (true && i++ < 5 && get_status()!=BT::HALTED)
-//    {
-//        std::chrono::milliseconds(1000);
-//        cout << "Running!" << endl;
+    std::cout << "The Action is doing more operations" << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    if (is_halted())
+    {
+        return BT::HALTED;
+    }
 
-
-//    }
-    cout << "DONE!" << endl;
-
+    std::cout << "The Action has succeeded" << std::endl;
     return BT::SUCCESS;
 }
+
 void MyAction::Halt()
 {
 
 }
 
 
-
 int main(int argc, char *argv[])
 {
 
-    BT::SequenceNode* seq1 = new BT::SequenceNode("a");
-    BT::ConditionTestNode* con1 = new BT::ConditionTestNode("c1");
-    BT::ConditionTestNode* con2 = new BT::ConditionTestNode("c2");
-
+    BT::SequenceNode* seq = new BT::SequenceNode("Sequence");
     MyCondition* my_con_1 = new MyCondition("My condition");
     MyAction* my_act_1 = new MyAction("My action");
+    int tick_time_milliseconds = 1000;
 
-    seq1->AddChild(my_con_1);
-    seq1->AddChild(my_act_1);
+    seq->AddChild(my_con_1);
+    seq->AddChild(my_act_1);
 
-    Execute(seq1, 100);
+    Execute(seq, tick_time_milliseconds);
 
     return 0;
 }
